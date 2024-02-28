e168cb11100468b5c8e1bb93a579c3a8c9fb6f52
TheWalkingFroggy <72137010+TheWalkingFroggy@users.noreply.github.com>
2024-02-28 18:43:33 +0100

    Update randomizer.py
    - Added support for exclusive zone tracks
    - Optimized code

--- randomizer.py
+++ randomizer.py
@@ -3,8 +3,9 @@ import sys
 import os.path
 
 path = './dummyzonemode.effectsettings'
+pathdlc = './dummyzonemodedlc3.effectsettings'
 
-if(os.path.isfile(path)):
+def Randomizer():
     with open("dummyzonemode.effectsettings", "r") as f, open("zonemode.effectsettings", "w") as f1:
         for line in f:
             substr = line.split('=')
@@ -31,5 +32,43 @@ if(os.path.isfile(path)):
             for x in numericValues:
                 print(x, "", end="")
             print("")
+    return
 
+def RandomizerDLC():
+    with open("dummyzonemodedlc3.effectsettings", "r") as f, open("zonemodedlc3.effectsettings", "w") as f1:
+        for line in f:
+            substr = line.split('=')
+            numericValues = substr[1].split()
+
+            i = 0
+            while i<len(numericValues):
+                if((substr[0].find('Scene.Texture') != -1) or substr[0].find('Track.Base Colour Middle') != -1):
+                    numericValues[i] = numericValues[i].replace(numericValues[i], str(round(random.uniform(0, 3), 6)))
+                else:
+                    if((substr[0].find('Scene.Base Colour"') != -1) or (substr[0].find('Track.Texture') != -1)):
+                        numericValues[i] = numericValues[i].replace(numericValues[i], str(round(random.uniform(0, 2), 6)))
+                    else:
+                        if(substr[0].find('Scene.Base Colour Middle') != -1):
+                            numericValues[i] = numericValues[i].replace(numericValues[i], str(round(random.uniform(0.77, 2), 6)))
+                        else:
+                            if(substr[0].find('Track.Near') != -1):
+                                numericValues[i] = numericValues[i].replace(numericValues[i], str(round(random.uniform(0, 0.298039), 6)))
+                            else:
+                                if(substr[0].find('Track.Base Colour"') != -1):
+                                    numericValues[i] = numericValues[i].replace(numericValues[i], str(round(random.uniform(0, 0.933333), 6)))
+                i = i+1
+            
+            sys.stdout = f1
+            print(substr[0] + "=", end="")
+            for x in numericValues:
+                print(x, "", end="")
+            print("")
+    return
+
+if(os.path.isfile(path)):
+    Randomizer()
 else: print("zonemode.effectsettings file not found in directory. You sure you renamed it as 'dummyzonemode.effectsettings'?")
+
+if(os.path.isfile(path)):
+    RandomizerDLC()
+else: print("zonemodedlc3.effectsettings file not found in directory. You sure you renamed it as 'dummyzonemodedlc3.effectsettings'?")
